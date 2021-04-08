c1933d7dfccb63405e4e9cc43da1735d494f49cc
Jason <jrosasrjf@gmail.com>
2021-04-08 11:12:30 -0500

    added inquierer and got prompts to work in terminal

--- Develop/index.js
+++ Develop/index.js
@@ -1,21 +1,35 @@
 // TODO: Include packages needed for this application
 
 var fs = require("fs"); // Allows work with the file system. Can Read, create, update, delete, rename files. 
+var inquirer = require("inquirer");
+
+/*
+
+WHEN I enter a description, installation instructions, 
+usage information, contribution guidelines, and test instructions
+THEN this information is added to the sections of the README entitled 
+Description, Installation, Usage, Contributing, and Tests
+
+*/
+
 
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        message: "What is the App name?", 
-        name: "static-site"
+        type: "input", // sets the input type to a string user input. 
+        message: "What is the App name?", // prompt for the user in the command terminal 
+        name: "title" // where the message input is stored
     }, 
 
     {
+        type: "input",
         message: "How will the app be used?", 
         name: "description"
     }, 
 
     {
+        type: "input",
         message: "Who is the author?", 
         name: "author"
     }
@@ -30,16 +44,19 @@ const questions = [
     */
 ]
 
+function init() {
+    inquirer 
+    .prompt(questions) // prompts the array of questions
+    .then(answer => {
+        console.log(answer.title);
+    })
+
+}
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
-
-
-
-
-// TODO: Create a function to initialize app
-function init() {}
-
 
 
 // Function call to initialize app
