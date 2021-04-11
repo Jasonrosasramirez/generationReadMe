0631fbb620d64e18b9117332be594e9771fb06f2
Jason <jrosasrjf@gmail.com>
2021-04-11 16:40:25 -0500

    added in the remaining questions to the questions array

--- Develop/index.js
+++ Develop/index.js
@@ -5,39 +5,6 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown"); // links to the generatemarkdown.js file
 
 
-/*
-
-WHEN I enter a description, installation instructions, 
-usage information, contribution guidelines, and test instructions
-THEN this information is added to the sections of the README entitled 
-Description, Installation, Usage, Contributing, and Tests
-
-
-WHEN I am prompted for information about my application repository
-THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-
-WHEN I enter my project title
-THEN this is displayed as the title of the README
-
-WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-
-WHEN I choose a license for my application from a list of options
-THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
-WHEN I enter my GitHub username
-THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-
-WHEN I enter my email address
-THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-
-WHEN I click on the links in the Table of Contents
-THEN I am taken to the corresponding section of the README
-
-*/
-
-
-
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -68,20 +35,32 @@ const questions = [
     {
         type: "input",
         message: "How is the app installed?", 
-        name: "install"
+        name: "installation"
     }, 
 
     {
         type: "input",
-        message: "how are issues reported?", 
-        name: "issueReport"
+        message: "What is the usage unformation?", 
+        name: "usageInformation"
     }, 
 
     {
         type: "input",
         message: "How can contributions be made?", 
         name: "contributions"
-    } 
+    }, 
+    
+    {
+        type: "input",
+        message: "What is your gitHub username?", 
+        name: "questionsGithub"
+    }, 
+
+    {
+        type: "input",
+        message: "What is your email address?", 
+        name: "questionsEmail"
+    }
 
 ]
 
