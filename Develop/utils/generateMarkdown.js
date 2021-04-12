28ded56bbc04f8d0fe4f971a92b422fe7e1448f0
Jason <jrosasrjf@gmail.com>
2021-04-11 21:14:28 -0500

    commeted in what the popular license terms are

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,21 +1,28 @@
+
 // TODO: Create a function that returns a license badge based on which license is passed in
-
-
-
 function renderLicenseBadge(license) {
-
-
-
   // If there is no license, return an empty string
+
+
+  // "MIT", "GPLv2", "Apache", "No License"
+
 }
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  // If there is no license, return an empty string
+
+
+
+}
 
 // TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  // If there is no license, return an empty string
+
+
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -24,8 +31,6 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown; // functions must always be exported before working in other scripts
 
-
-
+module.exports = generateMarkdown; // functions must always be exported before working in other scripts. In this case, the other three functions will be called within markdown. 
 // function license badge, link, sections are independant functions that will be called in generateMarkdown. 
