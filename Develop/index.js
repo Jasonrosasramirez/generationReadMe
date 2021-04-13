3a4c895875c125ffce9e29a66eadbcbed5710dfa
Jason <jrosasrjf@gmail.com>
2021-04-12 19:39:34 -0500

    added comments to the index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -2,17 +2,18 @@
 
 const fs = require("fs"); // Allows work with the file system. Can Read, create, update, delete, rename files. 
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown"); // links to the generatemarkdown.js file
+const generateMarkdown = require("./utils/generateMarkdown"); // links to the generatemarkdown.js file | Specifically the generatemarkdown function. 
 
 
-// TODO: Create an array of questions for user input
+// Create an array of question objects for user. These are the questions and answer pairs. 
 const questions = [
     // Contains all of the prompts that will be asked. 
+    // https://www.npmjs.com/package/inquirer for reference
 
-    {
+    { // First question that will be asked
         type: "input", // sets the input type to a string user input. 
         message: "What is the App name?", // prompt for the user in the command terminal 
-        name: "title" // where the message input is stored as a hash. Can be assessed by referencing answers.
+        name: "title" // where the message input is stored as a hash. Can be assessed by referencing answers.title later on in the code
     }, 
 
     {
@@ -28,7 +29,7 @@ const questions = [
     }, 
 
     {
-        type: "list", // A list here will come out as a string. Using checklist will output results as an array
+        type: "list", // A list here will come out as a string. Using checklist will output results as an array, which is more work. 
         message: "choose your license", 
         name: "license", 
         choices: ["MIT", "GPLv2", "Apache", "No License"]  
@@ -71,10 +72,10 @@ const questions = [
         name: "questionsEmail"
     }
 
-]
+] // referenced in Init()
 
 
-// Writes the README file. 
+// This is what actually writes/updates the README file. 
 function writeToFile(fileName, template) {
 
   //fs.writeFile(fileName, data, [encoding], [callback])
@@ -82,10 +83,14 @@ function writeToFile(fileName, template) {
         // file = (string) | filepath to file 
         // data = (string or buffer) | what you will write into the file
         // encoding = (optional string) | utf8 is assumed if no encoding provided
-        // callback = (function (err)) | This function
+        // callback = (function (err)) | This function right here 
+
+        /*
+        for debugging if needed 
 
         console.log(fileName); 
         console.log(template);
+        */
 
         if (err) {
             return console.log(err); 
@@ -97,23 +102,25 @@ function writeToFile(fileName, template) {
         // https://nodejs.org/en/knowledge/file-system/how-to-write-files-in-nodejs/
     })
 
-} // write to file complete. 
+} // Referenced in Init(). Init feeds in the desired information to this function
 
-
+// I bring it all together :D 
 function init() {
-    // this brings it all together. 
-
+    
     inquirer 
     .prompt(questions) // prompts the array of questions on the terminal
     .then((answer) => { // a promise made here. Answers access the hash where the name is stored. 
         
-        // debugger
+        /*
+        debugger
         console.log("\n" + answer.title); // prints out what the title is. Access the hash using answer. 
         console.log(answer.description);
-
+        */ 
+        
         var readme = generateMarkdown(answer); // represents the template within the generateMarkdown.js script as a variable. The answer hash is then used as the data parameter
         console.log(readme); // prints the variable. What is represented here, will be printed on the readme file. 
         writeToFile("./generateHere/README.md", readme);  // the desired readme directory - must update the readme itself, what we will write to the actual readme using file system. 
+        // The write to file function is updated with the template to the readme template 
 
     })
 
@@ -121,10 +128,11 @@ function init() {
 
 
 // Function call to initialize app
-init(); // parameters will be name description, etc.  
+init(); 
 
 
-// references
+
+/* references */ 
 
 // https://nodejs.org/en/knowledge/file-system/how-to-write-files-in-nodejs/
 // https://www.npmjs.com/package/inquirer
